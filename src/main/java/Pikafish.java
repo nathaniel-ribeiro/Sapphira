c07a3772e441a0b4bdd4286697148ae90998dcc5
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2026-01-02 14:41:27 -0500

    bugfix to best move

--- src/main/java/Pikafish.java
+++ src/main/java/Pikafish.java
@@ -3,8 +3,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class Pikafish {
-    private final Process process;
     private final BufferedReader bufferedReader;
+    private final PrintWriter printWriter;
     private static Pikafish INSTANCE;
 
     private static final int NUM_THREADS = 15;
@@ -14,8 +14,10 @@ public final class Pikafish {
     private Pikafish(final String pathToExecutable){
         final ProcessBuilder processBuilder = new ProcessBuilder(pathToExecutable);
         try{
-            this.process = processBuilder.start();
-            this.bufferedReader = new BufferedReader(new InputStreamReader(this.process.getInputStream()));
+            final Process process = processBuilder.start();
+            this.bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            final OutputStream outputStream = process.getOutputStream();
+            this.printWriter = new PrintWriter(outputStream);
         }
         catch(final IOException exception){
             throw new RuntimeException(exception);
@@ -33,26 +35,32 @@ public final class Pikafish {
      * @param keyword Keyword to wait for
      */
     private void waitForResponseContaining(final String keyword) {
-        final String ignored = this.bufferedReader.lines()
-                                                .filter(line -> line.contains(keyword))
-                                                .findFirst()
-                                                .orElseThrow(RuntimeException::new);
+        try {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.contains(keyword)) {
+                    return;
+                }
+            }
+            throw new RuntimeException("Process terminated before keyword: '" + keyword + "' was received.");
+        } catch (final IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
+
     private void sendCommand(final String command){
-        final OutputStream outputStream = this.process.getOutputStream();
-        final PrintWriter printWriter = new PrintWriter(outputStream);
         printWriter.println(command);
         printWriter.flush();
     }
 
     public Move getBestMove(final Board board) throws IOException {
-        this.sendCommand("set position fen " + board.getFen());
+        this.sendCommand("position fen " + board.getFen());
         this.sendCommand("go nodes " + NODES_TO_SEARCH);
         String line;
         while ((line = this.bufferedReader.readLine()) != null) {
             if (line.startsWith("bestmove")) {
-                final Pattern pattern = Pattern.compile("bestmove ([a-i]\\d)([a-i]\\d).+");
+                final Pattern pattern = Pattern.compile("bestmove ([a-i]\\d)([a-i]\\d)(?:\\s+.*)?");
                 final Matcher matcher = pattern.matcher(line);
                 final boolean b = matcher.matches();
                 // group 0 is the whole line, group 1 is the best move group
