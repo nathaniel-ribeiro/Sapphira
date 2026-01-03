3c7b9a7a2d4c9a8ef18d603bfc7492043c76c6e2
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2026-01-02 20:15:39 -0500

    added evaluation code (UNTESTED)

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,17 +4,12 @@ import java.util.List;
 public final class Driver {
     public static void main(final String[] args) throws IOException {
         final Pikafish pikafish = new Pikafish(ConfigOptions.INSTANCE);
-        final XiangqiRulesService xqRules = new XiangqiRulesService(pikafish);
-        final Board board = xqRules.makeMoves(
+        final Board board = pikafish.makeMoves(
                 Board.STARTING_BOARD,
                 List.of(
-                        new Move("h2", "e2"),
-                        new Move("h9", "g7"),
-                        new Move("h0", "g2"),
-                        new Move("i9", "h9"),
-                        new Move("g3", "g4")
+                        new Move("h2", "h9")
                 )
         );
-        System.out.println(pikafish.getBestMove(board));
+        System.out.println(pikafish.evaluate(board));
     }
 }
--- src/main/java/Pikafish.java
+++ src/main/java/Pikafish.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class Pikafish {
+public final class Pikafish{
     private final BufferedReader reader;
     private final PrintWriter writer;
     private final int nodesToSearch;
@@ -19,6 +19,8 @@ public final class Pikafish {
 
     private static final Pattern BEST_MOVE_PATTERN = Pattern.compile("bestmove ([a-i]\\d)([a-i]\\d)(?:\\s+.*)?");
     private static final Pattern FEN_EXTRACTOR_PATTERN = Pattern.compile("Fen: (.+)");
+    private static final Pattern LEGAL_MOVE_PATTERN = Pattern.compile("([a-i]\\d)([a-i]\\d): 1");
+    private static final Pattern SCORE_CP_PATTERN = Pattern.compile(".*score cp (-?M?(\\d+)).*");
 
     public Pikafish(final PikafishOptions options){
         if(options.getNumThreads() < MIN_THREADS || options.getNumThreads() > MAX_THREADS)
@@ -34,18 +36,21 @@ public final class Pikafish {
         try{
             final Process process = processBuilder.start();
             this.reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            final OutputStream outputStream = process.getOutputStream();
-            this.writer = new PrintWriter(outputStream, true);
+            this.writer = new PrintWriter(process.getOutputStream(), true);
         }
         catch(final IOException exception){
             throw new RuntimeException(exception);
         }
-        this.writer.println("uci");
+        this.send("uci");
         final String ignored1 = this.waitForResponseContaining("uciok");
-        this.writer.println("isready");
+        this.send("isready");
         final String ignored2 = this.waitForResponseContaining("readyok");
-        this.writer.println("setoption name Threads value " + options.getNumThreads());
-        this.writer.println("setoption name Hash value " + options.getHashSizeMiB());
+        this.send("setoption name Threads value " + options.getNumThreads());
+        this.send("setoption name Hash value " + options.getHashSizeMiB());
+    }
+
+    private void send(final String command){
+        this.writer.println(command);
     }
 
     /**
@@ -67,9 +72,9 @@ public final class Pikafish {
         }
     }
 
-    public Move getBestMove(final Board board) throws IOException {
-        this.writer.println("position fen " + board.getFen());
-        this.writer.println("go nodes " + this.nodesToSearch);
+    public Move getBestMove(final Board board){
+        this.send("position fen " + board.getFen());
+        this.send("go nodes " + this.nodesToSearch);
         final String bestMoveLine = this.waitForResponseContaining("bestmove");
         final Matcher matcher = BEST_MOVE_PATTERN.matcher(bestMoveLine);
         final boolean b = matcher.matches();
@@ -79,13 +84,18 @@ public final class Pikafish {
         return new Move(srcSquare, destSquare);
     }
 
+    public Board makeMove(final Board board, final Move move){
+        return this.makeMoves(board, List.of(move));
+    }
+
     public Board makeMoves(final Board board, final List<Move> moves){
+        if(moves.isEmpty()) return board;
         final String movesString = String.join(" ", moves.stream().map(Move::toString).toList());
-        this.writer.println("position fen " + board.getFen() + " moves " + movesString);
+        this.send("position fen " + board.getFen() + " moves " + movesString);
         // NOTE: this is a Stockfish/Pikafish specific command to display the board/get the final FEN.
         // It is NOT a command guaranteed by the UCI protocol.
         // For compatibility with other UCI engines, this code should be changed.
-        this.writer.println("d");
+        this.send("d");
         final String fenLine = this.waitForResponseContaining("Fen:");
         final Matcher matcher = FEN_EXTRACTOR_PATTERN.matcher(fenLine);
         final boolean b = matcher.matches();
@@ -94,16 +104,58 @@ public final class Pikafish {
         return new Board(fen);
     }
 
-    public int evaluate(final Board board){
-        this.writer.println("position fen " + board.getFen());
-        this.writer.println("go nodes " + this.nodesToSearch);
-        //TODO
-        return 0;
+    public double evaluate(final Board board) {
+        this.send("position fen " + board.getFen());
+        this.send("go nodes " + this.nodesToSearch);
+        int evaluationCentipawns = 0;
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                final Matcher matcher = SCORE_CP_PATTERN.matcher(line);
+                if (line.contains("bestmove")) break;
+                if (matcher.matches()) {
+                    try {
+                        evaluationCentipawns = Integer.parseInt(matcher.group(1));
+                    } catch (final NumberFormatException exception) {
+                        // this catch block will proc if the evaluation is checkmate-in-n rather than centipawns
+                        if (matcher.group(1).startsWith("M")) {
+                            final int pliesTilMate = Integer.parseInt(matcher.group(2));
+                            // we would rather make haste if we are doing the checkmating
+                            evaluationCentipawns = 10_000 - pliesTilMate;
+                        } else if (matcher.group(1).startsWith("-M")) {
+                            final int pliesTilMate = Integer.parseInt(matcher.group(2));
+                            // we would rather delay the inevitable if we are getting checkmated
+                            evaluationCentipawns = -10_000 + pliesTilMate;
+                        } else throw new RuntimeException();
+                    }
+                }
+            }
+        } catch (final IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        // convert to pawns
+        return evaluationCentipawns / 100.0;
     }
 
     public List<Move> getLegalMoves(final Board board) {
-        //TODO
-        List<Move> moves = new ArrayList<>();
+        this.send("position fen " + board.getFen());
+        // NOTE: this is a Stockfish/Pikafish specific command to display the board/get the final FEN.
+        // It is NOT a command guaranteed by the UCI protocol.
+        // For compatibility with other UCI engines, this code should be changed.
+        this.send("go perft 1");
+        final List<Move> moves = new ArrayList<>();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                final Matcher matcher = LEGAL_MOVE_PATTERN.matcher(line);
+                if(line.contains("Nodes searched")) break;
+                if (matcher.matches()) {
+                    moves.add(new Move(matcher.group(1), matcher.group(2)));
+                }
+            }
+        } catch (final IOException exception) {
+            throw new RuntimeException(exception);
+        }
         return ImmutableList.copyOf(moves);
     }
 }
