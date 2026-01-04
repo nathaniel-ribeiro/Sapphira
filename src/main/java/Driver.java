cbd83d4aeed0980faa4e29f35c27d7621e790cd9
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2026-01-04 16:28:54 -0500

    refactor for clarity

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -7,12 +6,7 @@ import java.util.stream.IntStream;
 public final class Driver {
     public static void main(final String[] args){
         final Pikafish pikafish = new Pikafish(ConfigOptions.INSTANCE);
-        final Board board = pikafish.makeMoves(Board.STARTING_BOARD,
-                List.of(
-                        new Move("h2", "e2"),
-                        new Move("h9", "g7")
-                )
-        );
+        final Board board = Board.STARTING_BOARD;
         final List<Move> allLegalMoves = pikafish.getLegalMoves(board);
         final List<Double> evaluations = allLegalMoves.stream()
                                         .map(move -> pikafish.makeMove(board, move))
@@ -23,6 +17,11 @@ public final class Driver {
         final Map<Move, Double> moveEvaluations = IntStream.range(0, allLegalMoves.size()).boxed().collect(Collectors.toMap(allLegalMoves::get, evaluations::get));
         final IPRParameters parameters = new IPRParameters(0.139, 0.454);
         final IPRModel model = new IPRModel(parameters);
-        System.out.println(model.getMoveProbabilities(moveEvaluations));
+        final Map<Move, Double> map = model.getProjectedMoveProbabilities(moveEvaluations);
+
+        System.out.println("Central cannon: " + map.get(new Move("h2", "e2")));
+        System.out.println("Soldier opening: " + map.get(new Move("c3", "c4")));
+        System.out.println("Flying elephant: " + map.get(new Move("g0", "e2")));
+        System.out.println("Palcorner cannon: " + map.get(new Move("h2", "f2")));
     }
 }
--- src/main/java/IPRModel.java
+++ src/main/java/IPRModel.java
@@ -1,7 +1,9 @@
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.apache.commons.math3.analysis.UnivariateFunction;
-import org.apache.commons.math3.analysis.solvers.BisectionSolver;
+import org.apache.commons.math3.analysis.solvers.BracketingNthOrderBrentSolver;
+import org.apache.commons.math3.analysis.solvers.BrentSolver;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -10,10 +12,15 @@ import java.util.stream.IntStream;
 
 public final class IPRModel {
     private final IPRParameters parameters;
+
+    private static final double MIN_BEST_MOVE_PROJECTED_PROBABILITY = 0.0;
+    private static final double MAX_BEST_MOVE_PROJECTED_PROBABILITY = 1.0;
+    private static final int MAX_ROOTFINDER_ITERATIONS = 100;
+
     public IPRModel(final IPRParameters parameters){
         this.parameters = parameters;
     }
-    public Map<Move, Double> getMoveProbabilities(final Map<Move, Double> moveEvaluations){
+    public Map<Move, Double> getProjectedMoveProbabilities(final Map<Move, Double> moveEvaluations){
         final List<Entry<Move, Double>> moveEvaluationsSorted = moveEvaluations.entrySet()
                                                                          .stream()
                                                                          .sorted(Entry.<Move, Double>comparingByValue().reversed())
@@ -21,35 +28,32 @@ public final class IPRModel {
 
         final List<Move> moves = moveEvaluationsSorted.stream().map(Entry::getKey).toList();
         final List<Double> evaluations = moveEvaluationsSorted.stream().map(Entry::getValue).toList();
-        final double v_best = evaluations.get(0);
-        final List<Double> deltas = evaluations.stream().map(v_i -> {
+        final double bestEvaluation = evaluations.get(0);
+        final List<Double> deltas = evaluations.stream().map(evaluation -> {
             final UnivariateFunction antiderivative = z -> (z * Math.log1p(Math.abs(z))) / Math.abs(z);
-            final double upper = antiderivative.value(v_best);
-            final double lower = antiderivative.value(v_i);
+            final double upper = antiderivative.value(bestEvaluation);
+            final double lower = antiderivative.value(evaluation);
             return upper - lower;
         }).toList();
         final List<Double> alphas = deltas.stream()
-                .map(delta_i -> Math.exp(Math.pow((delta_i / parameters.sensitivity()), parameters.consistency())))
+                .map(delta -> Math.exp(-Math.pow((delta / parameters.sensitivity()), parameters.consistency())))
                 .toList();
-        final List<Double> probabilities = this.normalize(alphas);
-        final Map<Move, Double> moveProbabilities = IntStream.range(0, moves.size()).boxed().collect(Collectors.toMap(moves::get, probabilities::get));
-        return ImmutableMap.copyOf(moveProbabilities);
+        final List<Double> projectedProbabilities = this.normalize(alphas);
+        final Map<Move, Double> projectedMoveProbabilities = IntStream.range(0, moves.size()).boxed().collect(Collectors.toMap(moves::get, projectedProbabilities::get));
+        return ImmutableMap.copyOf(projectedMoveProbabilities);
     }
 
     private List<Double> normalize(final List<Double> alphas){
         // each p_i = p_best ^ {\alpha_i}
         // constraint: \sum p_i = 1 (probability vector)
-        final UnivariateFunction function = p_best -> {
-            double sum = 0;
-            for(final double alpha : alphas){
-                sum += Math.pow(p_best, alpha);
-            }
-            sum = sum - 1;
-            return sum;
-        };
-        final BisectionSolver solver = new BisectionSolver();
-        final double p_best = solver.solve(1000, function, 0.0, 1.0);
-        final List<Double> probabilities = alphas.stream().map(alpha -> Math.pow(p_best, alpha)).toList();
-        return ImmutableList.copyOf(probabilities);
+        final UnivariateFunction function = guessForPBest -> alphas.stream().mapToDouble(alpha -> Math.pow(guessForPBest, alpha)).sum() - 1.0;
+        final BracketingNthOrderBrentSolver solver = new BracketingNthOrderBrentSolver();
+        final double pBest = solver.solve(MAX_ROOTFINDER_ITERATIONS,
+                                          function,
+                                          MIN_BEST_MOVE_PROJECTED_PROBABILITY,
+                                          MAX_BEST_MOVE_PROJECTED_PROBABILITY);
+
+        final List<Double> projectedProbabilities = alphas.stream().map(alpha -> Math.pow(pBest, alpha)).toList();
+        return ImmutableList.copyOf(projectedProbabilities);
     }
 }
