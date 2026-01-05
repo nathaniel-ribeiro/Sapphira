001a6a40bd085c2e26c71280397db9c6d7e5b698
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2026-01-05 14:04:49 -0500

    migration to Kotlin

--- src/main/java/Move.kt
+++ src/main/java/Move.kt
@@ -1,25 +1,24 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
-public record Move(String srcSquare, String destSquare) {
-    private static final Pattern SQUARE_PATTERN = Pattern.compile("[a-i]\\d");
-    public Move(final String srcSquare, final String destSquare) {
+private val SQUARE_PATTERN: Pattern = Pattern.compile("[a-i]\\d")
+class Move(srcSquare: String, destSquare: String) {
+    val srcSquare: String
+    val destSquare: String
+
+    init {
         //NOTE: this is a necessary but insufficient check for legal moves
-        Matcher matcher = SQUARE_PATTERN.matcher(srcSquare);
-        if (!matcher.matches())
-            throw new IllegalArgumentException("Source square must be a-i followed by a number (e.g. i0).");
-        matcher = SQUARE_PATTERN.matcher(destSquare);
-        if (!matcher.matches())
-            throw new IllegalArgumentException("Destination square must be a-i followed by a number (e.g. i0).");
-        if (srcSquare.equalsIgnoreCase(destSquare))
-            throw new IllegalArgumentException("Source square cannot be the same as destination square.");
-
-        this.srcSquare = srcSquare.toLowerCase();
-        this.destSquare = destSquare.toLowerCase();
+        var matcher: Matcher = SQUARE_PATTERN.matcher(srcSquare)
+        require(matcher.matches()) { "Source square must be a-i followed by a number (e.g. i0)." }
+        matcher = SQUARE_PATTERN.matcher(destSquare)
+        require(matcher.matches()) { "Destination square must be a-i followed by a number (e.g. i0)." }
+        require(!srcSquare.equals(destSquare, ignoreCase = true)) { "Source square cannot be the same as destination square." }
+        this.srcSquare = srcSquare.lowercase()
+        this.destSquare = destSquare.lowercase()
     }
 
-    @Override
-    public String toString() {
-        return this.srcSquare + this.destSquare;
+    override fun toString(): String {
+        return this.srcSquare + this.destSquare
     }
 }
