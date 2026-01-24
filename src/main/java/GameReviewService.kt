29b7af965c148d4f47ef4623701347d91b91442d
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2026-01-24 11:51:43 -0500

    removed TODO

--- src/main/java/GameReviewService.kt
+++ src/main/java/GameReviewService.kt
@@ -1,16 +1,12 @@
 class GameReviewService(val pikafish: Pikafish) {
-    //TODO: this currently makes 3x calls to Pikafish evaluate than strictly necessary.
-    // 1x call to get best move
-    // 1x call to get evaluation of best move
-    // 1x call to get evaluation of move played
-    // best move can be extracted from the same call to evaluate, evaluation of best move can be extracted from evaluation of the last board
     fun review(game: Game): ReviewedGame {
         var curBoard = Board.STARTING_BOARD
         val reviewedMoves = ArrayList<ReviewedMove>()
         for((i, move) in game.moves.withIndex()){
             val bestMove = pikafish.getBestMove(curBoard)
-            val bestMoveEvaluation = if(i >= 1)
-                reviewedMoves.map(ReviewedMove::movePlayedEvaluation)[i-1].flip() else pikafish.evaluate(curBoard)
+            val bestMoveEvaluation =
+                if(i == 0) pikafish.evaluate(curBoard)
+                else reviewedMoves.map(ReviewedMove::movePlayedEvaluation)[i-1].flip()
             curBoard = pikafish.makeMove(curBoard, move)
             val movePlayedEvaluation = pikafish.evaluate(curBoard).flip()
             reviewedMoves.add(ReviewedMove(move, bestMove, movePlayedEvaluation, bestMoveEvaluation))
