fc54d9fd89f91f373e6eb11cc65c4a772bf70fcf
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2026-03-23 17:18:26 -0400

    filtered out non 10 + 0 games

--- src/main/java/TrainingDataCleaningService.kt
+++ src/main/java/TrainingDataCleaningService.kt
@@ -6,11 +6,13 @@ class TrainingDataCleaningService {
         val notTooShortOrLong = game.moves.size in MIN_PLIES..MAX_PLIES
         val usernamesSufficientlyDifferent = JaroWinklerSimilarity().apply(game.redPlayer.username, game.blackPlayer.username) < MAX_USERNAME_SIMILARITY
         val isTimed = !game.isUntimed
+        val isRapidGame = game.gameTimer == 10 && game.moveTimer == 2 && game.increment == 0
 
         return neitherPlayerGuest &&
                notTooShortOrLong &&
                usernamesSufficientlyDifferent &&
-               isTimed
+               isTimed &&
+               isRapidGame
     }
 
     companion object {
