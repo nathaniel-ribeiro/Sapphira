c286ee5a1ac1ec332b1ef95c5627f5aa75d70b66
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2026-03-25 22:55:57 -0400

    stubbed out feature calculation tests

--- src/test/java/FeatureTest.kt
+++ src/test/java/FeatureTest.kt
@@ -1,2 +1,61 @@
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+
 class FeatureTest {
+    private val reviewedGame = mockk<ReviewedGame>()
+
+    @Test
+    fun accuracy_test(){
+        //TODO
+    }
+
+    @Test
+    fun rating_test(){
+        //TODO
+    }
+
+    @Test
+    fun adjusted_cp_loss_test(){
+        //TODO
+    }
+
+    @Test
+    fun blunder_rate_test(){
+        //TODO
+    }
+
+    @Test
+    fun longest_streak_best_or_excellent_past_opening_test(){
+        //TODO
+    }
+
+    @Test
+    fun evaluation_after_opening_test(){
+        //TODO
+    }
+
+    @Test
+    fun think_time_median_test(){
+        //TODO
+    }
+
+    @Test
+    fun think_time_iqr_test(){
+        //TODO
+    }
+
+    @Test
+    fun think_time_outlier_fraction_test(){
+        //TODO
+    }
+
+    @Test
+    fun accuracy_of_longest_think_test(){
+        //TODO
+    }
+
+    @Test
+    fun move_number_of_longest_think_test(){
+        //TODO
+    }
 }
